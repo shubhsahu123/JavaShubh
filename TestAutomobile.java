0f14e50982112c68c2294c82f0b49b38f2679829
shubhsahu123 <shubhsahu5759@gmail.com>
2022-07-13 16:32:07 +0530

    msg

---
+++ TestAutomobile.java
@@ -0,0 +1,18 @@
+package com.rays.basics;
+
+public class TestAutomobile {
+public static void main(String[] args) {
+	
+
+	 Automobile a=new Automobile();
+	 
+	 a.setColor("blue");
+	 
+	 System.out.println("  bike color is   "+a.getColor());
+	 
+	 a.setSpeed(40);
+	 System.out.println("current speed is"+a.getSpeed());
+	 
+	 
+	 
+}}
